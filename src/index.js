709c978cdd0877a031a078a25b2b734dedec413b
quyenptt03 <quyenptt03@gmail.com>
2022-11-04 21:51:42 +0700

    refacter code

--- src/index.js
+++ src/index.js
@@ -5,21 +5,18 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const route = require("./routes");
 
-app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(morgan("combined"));
 
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+//route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
