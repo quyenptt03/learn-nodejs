c87fdd3d0aad93a2e8d2da9c71a5aa92abe93834
quyenptt03 <quyenptt03@gmail.com>
2022-11-07 21:17:33 +0700

    MVC controler and  model

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
-const app = express();
-const port = 3000;
 
 const route = require("./routes");
+const db = require("./config/db");
+
+// connect
+db.connect();
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -13,11 +18,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set("views", path.join(__dirname, "resources", "views"));
 
 //route init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
